9f1e10696c05f57c85138e3396fe517c14af31c1
Ange Ndoudi <ericmrleliko@gmail.com>
2024-09-10 23:31:22 +0200

    Implementation with v-data-table

--- components/MusicTable.vue
+++ components/MusicTable.vue
@@ -1,52 +1,83 @@
-<script setup lang="ts">
+<script lang="js">
+  export default {
 
-    const {data, pending, error} = await useFetch(() => `${STRAPI_API_URL}/musics`, {
+    data () {
+      return {
+        headers: [
+          { title: 'play', key: 'play' },
+          { title: 'title', key: 'title' },
+          { title: 'length', key: 'length' },
+          { title: 'tags', key: 'tags' },
+          { title: 'download', key: 'download' },
+        ],
+
+        songs: []
+      }
+    },
+    async mounted() {
+      this.getSongs()
+    },
+    methods: {
+      async getSongs(){
+        const {data, pending, error} = await useFetch(() => `${STRAPI_API_URL}/musics`, {
       "headers": {
         "Authorization": `Bearer ${STRAPI_TOKEN_MUSIC}`
       }
     })
 
-    let songs = []
-    for (let item of data.value.data){
-        songs.push({id: item.id, ...item.attributes})
-    }
+          for (let item of data.value.data){
+              this.songs.push({id: item.id, ...item.attributes})
+          }
 
+          console.log(this.songs)
+        },
 
+        async downloadSongs(item){
+          // Create an invisible anchor element programmatically
+          const link = document.createElement('a');
+          link.href = item.s3_url; // Set href to the audio file URL
+          link.setAttribute('download', item.title); // Set the download attribute with the file name
+
+          // Append the link to the document body
+          document.body.appendChild(link);
+
+          // Programmatically click the link to trigger the download
+          link.click();
+
+          // Remove the link from the document
+          document.body.removeChild(link);
+        }
+
+      
+      }
+  }
 
 </script>
 
 <template>
-    <v-table>
-        <thead>
-      <tr>
-        <th class="text-left">
-          Play
-        </th>
-        <th class="text-left">
-          Title
-        </th>
-        <th class="text-left">
-          Time
-        </th>
-        <th class="text-left">
-          Tags
-        </th>
-      </tr>
-    </thead>
-    <tbody>
+  <v-data-table-virtual
+    :headers="headers"
+    :items="songs"
+    height="400"
+    width="20"
+  >
+  <template v-slot:item.play="{ item }">
+        <AudioPlayer :audio-source="item.s3_url">
+        </AudioPlayer>
+      </template>
 
-        <tr
-        v-for="item in songs"
-        :key="item.id"
-      >
-        <td>
-            <AudioPlayer :audioSource="item.s3_url">
-            </AudioPlayer>
-        </td>
-        <td>{{ item.title }}</td>
-        <td>{{ item.length }}</td>
-        <td>{{ item.tags }}</td>
-      </tr>
-    </tbody>
-    </v-table>
+  <template v-slot:item.download="{ item }">
+    <!-- Download Button -->
+    <v-btn class="ma-2"
+        color="red-lighten-2"
+        icon="mdi-download"
+        variant="text"
+        @click="downloadSongs(item)"
+        >
+    </v-btn>
+  </template>
+
+</v-data-table-virtual>
+
+
 </template>
