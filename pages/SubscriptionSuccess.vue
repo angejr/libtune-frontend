9f7ff376dd9a5ddd3a50a90985370178cda32f91
Ange Ndoudi <ericmrleliko@gmail.com>
2024-11-06 20:56:56 +0100

    Simplified subscription page

--- pages/SubscriptionSuccess.vue
+++ pages/SubscriptionSuccess.vue
@@ -3,68 +3,64 @@ const authStore = useAuthStore();
 
 const params = new URLSearchParams(document.location.search);
 
-const subscriptionOver = ref(false);
-const subscribed = ref(false);
+const subscriptionOver = ref(!!authStore.user.customerId);
+const subscribed = ref(!!authStore.user.customerId);
 
 const checkoutSessionId = params.get("sessionId");
 
 // If the user is already a customer skip the subscription process
-if (authStore.user.customerId){
-    subscriptionOver.value = true;
-    subscribed.value = true;
+if (!subscribed.value) {
+  if (checkoutSessionId) {
+    const { data, error } = await useFetch(
+      `${STRAPI_URL}/strapi-stripe/retrieveCheckoutSession/${checkoutSessionId}`,
+      {
+        method: "GET",
+      }
+    );
 
-}
-else{
-    if (checkoutSessionId) {
-      const { data, error } = await useFetch(
-        `${STRAPI_URL}/strapi-stripe/retrieveCheckoutSession/${checkoutSessionId}`,
-        {
-          method: "GET",
-        }
-      );
-    
-      if (data.value) {
-        let customerId = data.value.customer;
-    
-        if (customerId) {
-          // Fetch self user
+    if (data.value) {
+      let customerId = data.value.customer;
+
+      if (customerId) {
+        // Fetch self user
+        try {
           await authStore.fetchUser();
-    
-          let { error } = await useFetch(
-            `${STRAPI_API_URL}/users/${authStore.user.id}`,
-            {
-              method: "PUT",
-              headers: { Authorization: `Bearer ${STRAPI_TOKEN_USER}` },
-              body: {
-                customerId,
-              },
-            }
-          );
-    
-          subscriptionOver.value = true;
-          if (error.value) {
-            alert(error.value.data.error.message);
-          } 
-          else {
-            subscribed.value = true;
-          }
-        } 
-        else {
-          subscriptionOver.value = true;
-          alert("No customer found");
+        } catch (e) {
+          alert(e.message);
         }
-      }
-    
-      if (error.value) {
-        subscriptionOver.value = true;
-        alert(error.value.data.error.message);
-      }
-    } else {
-      subscriptionOver.value = true;
-      subscribed.value = false;
-    }
-}
 
+        let { error } = await useFetch(
+          `${STRAPI_API_URL}/users/${authStore.user.id}`,
+          {
+            method: "PUT",
+            headers: { Authorization: `Bearer ${STRAPI_TOKEN_USER}` },
+            body: {
+              customerId,
+            },
+          }
+        );
+
+        subscriptionOver.value = true;
+        if (error.value) {
+          alert(error.value.data.error.message);
+        } else {
+          subscribed.value = true;
+        }
+      } else {
+        subscriptionOver.value = true;
+        alert("No customer found");
+      }
+    }
+
+    if (error.value) {
+      subscriptionOver.value = true;
+      alert(error.value.data.error.message);
+    }
+  } else {
+    subscriptionOver.value = true;
+    subscribed.value = false;
+  }
+}
 </script>
 
 <template>
