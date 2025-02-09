7ae6fe6c82a311ba1db3eedc0b8e974d672874c0
Ange Ndoudi <ericmrleliko@gmail.com>
2025-02-09 20:38:25 +0100

    Strapi stripe product ID is 2 not 1 since 1 is the test one

--- pages/Subscribe.vue
+++ pages/Subscribe.vue
@@ -68,7 +68,7 @@ function SS_ProductCheckout(productId, baseUrl, userEmail) {
 function goToStripe() {
   // for product Checkout
   try {
-    SS_ProductCheckout(1, STRAPI_URL, authStore.user.email);
+    SS_ProductCheckout(2, STRAPI_URL, authStore.user.email);
   } catch (e) {
     errorStore.setError({ title: "Error", text: e.message });
   }
