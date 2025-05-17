5a0dbea6d7bdc22570d3eb0dea789a1aae9c7bda
Ange Ndoudi <ericmrleliko@gmail.com>
2025-05-17 16:49:43 +0200

    Translated Acoount Page

--- pages/Account.vue
+++ pages/Account.vue
@@ -2,28 +2,27 @@
 const authStore = useAuthStore();
 const errorStore = useErrorStore();
 const displayStore = useDisplayStore();
+const { t, locales, setLocale } = useI18n();
 
 const subscriptionData = ref([]);
 const paymentMethods = ref([]);
 const payments = ref([]);
 
 useSeoMeta({
-  title: "Account",
-  ogtitle: "Account",
-  description:"Browse your libtune profile and account information",
-  oDescription:"Browse your libtune profile and account information",
+  title: t("Account.PageTitle"),
+  ogtitle: t("Account.PageTitle"),
+  description: t("Account.PageDescription"),
+  oDescription: t("Account.PageDescription"),
   robots: {
     noindex: true,
-    nofollow: true
-  }
-})
+    nofollow: true,
+  },
+});
 
-// Redirect to home if the user doesn't have an account yet
-if (!authStore?.userToken){
-  goToPath('/login')
+if (!authStore?.userToken) {
+  goToPath("/login");
 }
 
-// Fetching data from Stripe API
 if (authStore?.user?.customerId) {
   const { data, error } = await useFetch("/api/customers/me", {
     headers: {
@@ -32,7 +31,7 @@ if (authStore?.user?.customerId) {
   });
   if (error.value) {
     errorStore.setError({
-      title: "Could not fetch account data",
+      title: t("Account.FetchError"),
       texte: error.value.data,
     });
   } else {
@@ -49,108 +48,78 @@ const formatDate = (timestamp) =>
 </script>
 
 <template>
-  <v-container
-  class="py-8"
-    style="
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    "
-    fluid
-  >
-    <!-- Profile Section Section -->
+  <v-container class="py-8" fluid style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+    <!-- Profile -->
     <v-card outlined :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
-      <v-card-title class="text-h5"> Profile </v-card-title>
+      <v-card-title class="text-h5">{{ t("Account.Profile") }}</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
         <v-text-field
           v-model="authStore.user.email"
-          label="Email"
+          :label="t('Account.Email')"
           variant="outlined"
           disabled
           dense
           class="mt-4"
-        >
-        </v-text-field>
+        />
         <v-text-field
           v-model="authStore.user.username"
-          label="Username"
+          :label="t('Account.Username')"
           variant="outlined"
           disabled
           dense
           class="mt-4"
-        >
-        </v-text-field>
+        />
       </v-card-text>
     </v-card>
-    <!-- Subscriptions Section -->
+
+    <!-- Subscriptions -->
     <v-card outlined :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
-      <v-card-title class="text-h5">Subscriptions</v-card-title>
+      <v-card-title class="text-h5">{{ t("Account.Subscriptions") }}</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
         <v-data-table-virtual
           :headers="[
-            { title: 'Status', value: 'status', align: 'center' },
-            { title: 'Product', value: 'product' },
-            { title: 'Billing Frequency', value: 'billing_frequency' },
-            { title: 'Next Payment Date', value: 'next_payment_date' },
+            { title: t('Account.Status'), value: 'status', align: 'center' },
+            { title: t('Account.Product'), value: 'product' },
+            { title: t('Account.BillingFrequency'), value: 'billing_frequency' },
+            { title: t('Account.NextPaymentDate'), value: 'next_payment_date' },
             { title: '', value: 'action' },
           ]"
-          :items="
-            subscriptionData.map((sub) => ({
-              status: sub.status,
-              // product: sub.items.data[0]?.price.product.name,
-              product: 'Libtune Permium',
-              billing_frequency:
-                sub.items.data[0]?.price.recurring.interval === 'month'
-                  ? 'Monthly'
-                  : 'Yearly',
-              next_payment_date: formatDate(sub.current_period_end),
-              action,
-            }))
-          "
+          :items="subscriptionData.map((sub) => ({
+            status: sub.status,
+            product: 'Libtune Premium',
+            billing_frequency: sub.items.data[0]?.price.recurring.interval === 'month' ? t('Account.Monthly') : t('Account.Yearly'),
+            next_payment_date: formatDate(sub.current_period_end),
+            action,
+          }))"
         >
           <template v-slot:no-data>
             <div style="display: flex; justify-content: space-around">
-              <h2>No active subscription</h2>
-              <v-btn
-                @click="goToPath('/subscribe')"
-                variant="elevated"
-                color="purple"
-              >
-                BUY PREMIUM
+              <h2>{{ t("Account.NoSubscription") }}</h2>
+              <v-btn @click="goToPath('/subscribe')" variant="elevated" color="purple">
+                {{ t("Account.BuyPremium") }}
               </v-btn>
             </div>
           </template>
           <template v-slot:[`item.status`]="{ item }">
-            <v-chip v-if="item.status == 'active'" color="green" small>
-              active
+            <v-chip :color="item.status == 'active' ? 'green' : 'red'" small>
+              {{ item.status == 'active' ? t("Account.Active") : item.status }}
             </v-chip>
-            <v-chip v-else color="red" small> {{item.status}} </v-chip>
           </template>
-
           <template v-slot:[`item.product`]="{ item }">
-            <strong>
-              {{ item.product }}
-            </strong>
+            <strong>{{ item.product }}</strong>
           </template>
-
           <template v-slot:[`item.action`]="{}">
             <v-menu>
               <template v-slot:activator="{ props }">
-                <v-btn
-                  icon="mdi-dots-vertical"
-                  size="small"
-                  variant="plain"
-                  v-bind="props"
-                ></v-btn>
+                <v-btn icon="mdi-dots-vertical" size="small" variant="plain" v-bind="props" />
               </template>
               <v-list>
                 <v-list-item @click="goToPath('/unsubscribe')">
-                  <v-list-item-title style="color: red"
-                    >Cancel Subscription</v-list-item-title
-                  >
+                  <v-list-item-title style="color: red">
+                    {{ t("Account.CancelSubscription") }}
+                  </v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -159,64 +128,41 @@ const formatDate = (timestamp) =>
       </v-card-text>
     </v-card>
 
-    <!-- Payment Methods Section -->
-    <v-card v-if="authStore?.user?.customerId" outlined  :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
-      <v-card-title class="text-h5">Payment Methods</v-card-title>
+    <!-- Payment Methods -->
+    <v-card v-if="authStore?.user?.customerId" outlined :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
+      <v-card-title class="text-h5">{{ t("Account.PaymentMethods") }}</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
         <v-data-table-virtual
           :headers="[
             { title: '', value: 'brand' },
-            { title: 'Card Number', value: 'card_number' },
-            { title: 'Expiry Date', value: 'expiry_date' },
-            { title: 'Billing Details', value: 'actions', align: 'center' },
+            { title: t('Account.CardNumber'), value: 'card_number' },
+            { title: t('Account.ExpiryDate'), value: 'expiry_date' },
+            { title: t('Account.BillingDetails'), value: 'actions', align: 'center' },
           ]"
-          :items="
-            paymentMethods.map((method, index) => ({
-              brand: method.card.brand,
-              card_number: `**** **** **** ${method.card.last4}`,
-              expiry_date: `${method.card.exp_month}/${method.card.exp_year}`,
-              index,
-            }))
-          "
+          :items="paymentMethods.map((method, index) => ({
+            brand: method.card.brand,
+            card_number: `**** **** **** ${method.card.last4}`,
+            expiry_date: `${method.card.exp_month}/${method.card.exp_year}`,
+            index,
+          }))"
           item-value="index"
           item-expanded-value="index"
           show-expand
         >
           <template v-slot:[`item.brand`]="{ item }">
             <div style="display: flex; align-items: center">
-              <v-img
-                :src="`/images/${item.brand}.png`"
-                width="30"
-                height="30"
-              ></v-img>
-              <strong>
-                {{ item.brand }}
-              </strong>
+              <v-img :src="`/images/${item.brand}.png`" width="30" height="30" />
+              <strong>{{ item.brand }}</strong>
             </div>
           </template>
-
           <template v-slot:expanded-row="{ item }">
             <v-card flat>
               <v-card-text>
-                <p>
-                  <strong>Billing Name:</strong>
-                  {{ paymentMethods[item.index].billing_details.name }}
-                </p>
-                <p>
-                  <strong>Address:</strong>
-                  {{ paymentMethods[item.index].billing_details.address.line1 }}
-                </p>
-                <p>
-                  <strong>City:</strong>
-                  {{ paymentMethods[item.index].billing_details.address.city }}
-                </p>
-                <p>
-                  <strong>Country:</strong>
-                  {{
-                    paymentMethods[item.index].billing_details.address.country
-                  }}
-                </p>
+                <p><strong>{{ t("Account.BillingName") }}:</strong> {{ paymentMethods[item.index].billing_details.name }}</p>
+                <p><strong>{{ t("Account.Address") }}:</strong> {{ paymentMethods[item.index].billing_details.address.line1 }}</p>
+                <p><strong>{{ t("Account.City") }}:</strong> {{ paymentMethods[item.index].billing_details.address.city }}</p>
+                <p><strong>{{ t("Account.Country") }}:</strong> {{ paymentMethods[item.index].billing_details.address.country }}</p>
               </v-card-text>
             </v-card>
           </template>
@@ -224,43 +170,39 @@ const formatDate = (timestamp) =>
       </v-card-text>
     </v-card>
 
-    <!-- Payments Section -->
-    <v-card v-if="authStore?.user?.customerId" outlined  :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
-      <v-card-title class="text-h5">Payments</v-card-title>
+    <!-- Payments -->
+    <v-card v-if="authStore?.user?.customerId" outlined :class="displayStore.isMobile ? 'mb-4-mobile' : 'mb-4'">
+      <v-card-title class="text-h5">{{ t("Account.Payments") }}</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
         <v-data-table-virtual
           :headers="[
-            { title: 'Amount', value: 'amount' },
-            { title: 'Status', value: 'status' },
-            { title: 'Description', value: 'description' },
-            { title: 'Date', value: 'date' },
+            { title: t('Account.Amount'), value: 'amount' },
+            { title: t('Account.Status'), value: 'status' },
+            { title: t('Account.Description'), value: 'description' },
+            { title: t('Account.Date'), value: 'date' },
           ]"
-          :items="
-            payments.map((payment) => ({
-              status: payment.status,
-              amount: formatCurrency(payment.amount, payment.currency),
-              description: payment.description || 'No description provided',
-              date: formatDate(payment.created),
-            }))
-          "
+          :items="payments.map((payment) => ({
+            status: payment.status,
+            amount: formatCurrency(payment.amount, payment.currency),
+            description: payment.description || t('Account.NoDescription'),
+            date: formatDate(payment.created),
+          }))"
         >
           <template v-slot:[`item.status`]="{ item }">
             <v-chip :color="item.status == 'succeeded' ? 'green' : 'red'" small>
               {{ item.status }}
             </v-chip>
           </template>
-
           <template v-slot:[`item.amount`]="{ item }">
-            <strong>
-              {{ item.amount }}
-            </strong>
+            <strong>{{ item.amount }}</strong>
           </template>
         </v-data-table-virtual>
       </v-card-text>
     </v-card>
   </v-container>
 </template>
+
 
 <style scoped>
 .mb-4 {
