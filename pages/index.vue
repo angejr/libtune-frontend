1624aa621c01eeb1cbf39b845da8e536cbb3c92d
Ange Ndoudi <ericmrleliko@gmail.com>
2025-05-17 17:34:28 +0200

    Translated Index Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script setup>
-  useSeoMeta({
+const { t, locales, setLocale } = useI18n();
+
+useSeoMeta({
   title: "Libtune",
   ogTitle: "Libtune",
-  description: "Access a massive library of royalty free and copyright free music for podcasts, YouTube, social media and content creation. Ready to download. Affordable licensing. Subscribe now!",
-  ogDescription: "Access a massive library of royalty free and copyright free music for podcasts, YouTube, social media and content creation. Ready to download. Affordable licensing. Subscribe now!",
+  description: t('Home.PageDescription'),
+  ogDescription: t('Home.PageDescription'),
   ogSiteName: "Libtune",
 });
 </script>
