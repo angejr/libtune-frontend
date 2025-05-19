a9acfe2df848fd39c73f51a4e29712dc9f0453d9
Ange Ndoudi <ericmrleliko@gmail.com>
2025-05-19 16:02:38 +0200

    Added 'Explor' breadcrumb and sale banner for non mobile display

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -44,88 +44,102 @@ onMounted(() => {
 </script>
 
 <template >
-  <v-app-bar
-    v-if="!displayStore.isMobile"
-    elevation="10"
-    class="app-bar"
-    color="#1e1e2f"
-  >
-    <!-- Title Section -->
-    <v-app-bar-title class="app-bar-title">
-      <v-btn
-        @click="goToPath('/')"
-        variant="text"
-        size="large"
-        class="app-title-button"
-      >
-        Libtune
-        <v-chip
-          v-if="authStore?.user?.customerId"
-          size="x-small"
-          variant="elevated"
-          color="purple"
-          class="premium-chip"
+  <div v-if="!displayStore.isMobile">
+    <v-app-bar color="red" density="compact" scroll-behavior="fully-hide" :readonly="true">
+      <v-app-bar-title>
+        <div class="text-container" style="text-align: center;">
+          <v-icon icon="mdi-fire" />
+          {{$t('AppBar.sale')}}:     {{ formatTime(timeLeft) }}  
+        </div>
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-app-bar
+      elevation="10"
+      class="app-bar"
+      color="#1e1e2f"
+    >
+      <!-- Title Section -->
+      <v-app-bar-title class="app-bar-title">
+        <v-btn
+          @click="goToPath('/')"
+          variant="text"
+          size="large"
+          class="app-title-button"
         >
-          PREMIUM
-        </v-chip>
-      </v-btn>
-    </v-app-bar-title>
-
-    <!-- Breadcrumbs and Actions -->
-    <template v-slot:append>
-      <!-- Breadcrumbs -->
-      <v-breadcrumbs class="breadcrumbs">
-        <v-breadcrumbs-item
-          :title="$t('AppBar.pricing')"
-          :to="localePath('/subscribe')"
-        ></v-breadcrumbs-item>
-        <v-breadcrumbs-divider divider=" "></v-breadcrumbs-divider>
-        <v-breadcrumbs-item :title="$t('AppBar.about')" :to="localePath('/about')"></v-breadcrumbs-item>
-        <v-breadcrumbs-divider divider=" "></v-breadcrumbs-divider>
-        <v-breadcrumbs-item :title="$t('AppBar.terms')" :to="localePath('/terms')"></v-breadcrumbs-item>
-      </v-breadcrumbs>
-
-      <!-- Authentication Buttons or Menu -->
-      <div v-if="!authStore?.isAuthenticated" class="auth-buttons">
-        <v-btn @click="goToPath('/login')">{{$t('AppBar.login')}}</v-btn>
-        <v-btn @click="goToPath('/signup')" variant="outlined"> {{$t('AppBar.signup')}} </v-btn>
-      </div>
-
-      <v-menu v-else>
-        <template v-slot:activator="{ props }">
-          <v-btn
-            v-bind="props"
-            elevation="3"
-            icon="mdi-account"
-            class="user-menu-btn"
-          ></v-btn>
-        </template>
-        <v-list>
-          <v-list-item>
-            <v-list-item-title class="user-info">{{
-              authStore?.user?.username
-            }}</v-list-item-title>
-            <v-list-subheader class="user-email">{{
-              authStore?.user?.email
-            }}</v-list-subheader>
-          </v-list-item>
-          <v-list-item @click="goToPath('/account')" class="text-capitalize font-inter text-h6 font-weight-regular">
-            {{$t('AppBar.account')}}
-          </v-list-item>
-          <v-list-item
-            prepend-icon="mdi-logout-variant"
-            baseColor="red"
-            @click="
-              goToPath('/');
-              authStore.logout();
-            "
+          Libtune
+          <v-chip
+            v-if="authStore?.user?.customerId"
+            size="x-small"
+            variant="elevated"
+            color="purple"
+            class="premium-chip"
           >
-            <v-list-item-title>{{$t('AppBar.logout')}}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </template>
-  </v-app-bar>
+            PREMIUM
+          </v-chip>
+        </v-btn>
+      </v-app-bar-title>
+  
+      <!-- Breadcrumbs and Actions -->
+      <template v-slot:append>
+        <!-- Breadcrumbs -->
+        <v-breadcrumbs class="breadcrumbs">
+          <v-breadcrumbs-item
+            :title="$t('AppBar.browse')"
+            :to="localePath('/')"
+          ></v-breadcrumbs-item>
+          <v-breadcrumbs-divider divider=" "></v-breadcrumbs-divider>
+          <v-breadcrumbs-item
+            :title="$t('AppBar.pricing')"
+            :to="localePath('/subscribe')"
+          ></v-breadcrumbs-item>
+          <v-breadcrumbs-divider divider=" "></v-breadcrumbs-divider>
+          <v-breadcrumbs-item :title="$t('AppBar.about')" :to="localePath('/about')"></v-breadcrumbs-item>
+          <v-breadcrumbs-divider divider=" "></v-breadcrumbs-divider>
+          <v-breadcrumbs-item :title="$t('AppBar.terms')" :to="localePath('/terms')"></v-breadcrumbs-item>
+        </v-breadcrumbs>
+  
+        <!-- Authentication Buttons or Menu -->
+        <div v-if="!authStore?.isAuthenticated" class="auth-buttons">
+          <v-btn @click="goToPath('/login')">{{$t('AppBar.login')}}</v-btn>
+          <v-btn @click="goToPath('/signup')" variant="outlined"> {{$t('AppBar.signup')}} </v-btn>
+        </div>
+  
+        <v-menu v-else>
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              elevation="3"
+              icon="mdi-account"
+              class="user-menu-btn"
+            ></v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title class="user-info">{{
+                authStore?.user?.username
+              }}</v-list-item-title>
+              <v-list-subheader class="user-email">{{
+                authStore?.user?.email
+              }}</v-list-subheader>
+            </v-list-item>
+            <v-list-item @click="goToPath('/account')" class="text-capitalize font-inter text-h6 font-weight-regular">
+              {{$t('AppBar.account')}}
+            </v-list-item>
+            <v-list-item
+              prepend-icon="mdi-logout-variant"
+              baseColor="red"
+              @click="
+                goToPath('/');
+                authStore.logout();
+              "
+            >
+              <v-list-item-title>{{$t('AppBar.logout')}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+    </v-app-bar>
+  </div>
 
   <div v-else>
     <v-app-bar color="red" density="compact" scroll-behavior="fully-hide" :readonly="true">
@@ -319,7 +333,7 @@ onMounted(() => {
   overflow: hidden;
   text-overflow: ellipsis;
   display: block;
-  font-size: clamp(12px, 4.5vw, 24px);
+  font-size: clamp(12px, 4.5vw, 20px);
 }
 
 .font-inter {
