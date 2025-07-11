5264d2b42ce27057ac228b0ab4da1c692d4d0db8
Ange Ndoudi <ericmrleliko@gmail.com>
2025-07-11 09:43:42 +0200

    Changed offer date

--- utils/utils.ts
+++ utils/utils.ts
@@ -198,7 +198,7 @@ export const countryCodeToPrice: any = {
 
 }
 
-export const offerExpiryDate = '2025-07-10T00:00:00Z' // Summer offer
+export const offerExpiryDate = '2025-07-21T00:00:00Z' // Summer offer
 
 export const languageToVersionCombination = {
   en: ['v_3_9',
